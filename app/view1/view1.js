03b3a6da6cc9c29ad202c4cf67501557fd2e07b2
Yoshi Nagasaki <yoshinagasaki@yahoo.com>
2015-10-04 12:36:10 -0700

    start the items inventory stuff

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,9 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+  $scope.lineItems = [
+    "a credit card",
+    "a income"
+  ];
 }]);
