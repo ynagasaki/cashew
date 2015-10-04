e145d52196a3c8b3d30f33ea086866bd102c6c4d
Yoshi Nagasaki <yoshiyuk@yahoo-inc.com>
2015-10-04 00:09:19 -0700

    Reorganized test files

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,8 @@ module.exports = function(config){
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/view*/**/*.js',
+      'test/unit/**/*.js'
     ],
 
     autoWatch : true,
