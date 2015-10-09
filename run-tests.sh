b163e7afdc38c67df43f20c9f909cafe0b6b4f3c
Yoshi Nagasaki <yoshinagasaki@yahoo.com>
2015-10-08 23:17:53 -0700

    Need to keep command to update web-driver for buildwq

--- run-tests.sh
+++ run-tests.sh
@@ -7,12 +7,11 @@ trap "kill $SERVER_PID" 0 1 2 3 4 5 6 7 8 9 10
 echo "## Running jshint tests"
 node_modules/.bin/jshint app/ --config .jshintrc --exclude app/bower_components
 echo "(Done)"
-
+echo
 echo "## Running unit tests"
 node_modules/.bin/karma start karma.conf.js --no-auto-watch --single-run --reporters=dots --browsers=Firefox
 echo "(Done)"
-
+echo
 echo "## Running end-to-end tests"
 node_modules/.bin/protractor "test/e2e/protractor.conf.js" --browser=firefox
 echo "(Done)"
-
