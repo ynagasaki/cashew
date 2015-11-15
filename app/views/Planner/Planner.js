4d538630e48b2ed87ed7981b221aba0de20e981c
Yoshiyuki Nagasaki <yoshinagasaki@yahoo.com>
2015-11-14 23:48:24 -0800

    Add breakdown of monthly debt-vs-income

--- app/views/Planner/Planner.js
+++ app/views/Planner/Planner.js
@@ -13,14 +13,44 @@
 
   planner.controller('Planner', ['$scope', 'LineItemsService', function($scope, LineItemsService) {
     var me = this;
+    var getMonthlyAmountGraphParts = function(lineItems) {
+      var debt_amt = 0;
+      var pay_amt = 0;
+      var aside_amt = 0;
+      var total = 0;
+      lineItems.forEach(function(item) {
+        if (item.freq.per === 'yr' && item.freq.split) {
+          aside_amt += item.amount / 12;
+          return;
+        }
+        if (item.freq.per !== 'mo') {
+          return;
+        }
+        if (item.type === 'minus') {
+          debt_amt += item.amount;
+        } else if (item.type === 'plus') {
+          pay_amt += item.amount;
+        }
+      });
+      total = debt_amt + pay_amt + aside_amt;
+      return (total > 0) ? [
+        {color: '#FF4747', width: Math.round(debt_amt / total * 100)},
+        {color: '#FFDD45', width: Math.round(aside_amt / total * 100)},
+        {color: '#A5E85D', width: Math.round(pay_amt / total * 100)}
+      ] : [];
+    };
 
     me.lineItems = [];
 
     me.updateLineItems = function() {
       me.lineItems = LineItemsService.lineItems;
+      me.graphParts = getMonthlyAmountGraphParts(me.lineItems);
     };
     me.removeLineItem = function(item) {
       LineItemsService.remove(item);
+    };
+    me.getPartStyle = function(part) {
+      return 'background-color: ' + part.color + '; width: ' + part.width + '%';
     };
 
     $scope.$on('lineitems.added', me.updateLineItems);
