08a78d51145ff0e0ad6fdf4bf5dc34d75305b582
Yoshiyuki Nagasaki <yoshinagasaki@yahoo.com>
2016-02-14 12:28:40 -0800

    Added unit tests for closeable items logic changes

--- test/unit/services/LineItemsServiceTest.js
+++ test/unit/services/LineItemsServiceTest.js
@@ -30,5 +30,50 @@
         })
       );
     });
+
+    describe('LineItemsService', function() {
+      it('should assign today\'s date (as unix timestamp) as the end date of closed line items',
+        inject(function($httpBackend, LineItemsService) {
+          expect(LineItemsService).toBeDefined();
+          expect($httpBackend).toBeDefined();
+
+          $httpBackend.expect('PUT', /\/api\/update\/line-item/).respond(
+            function(method, url, data, headers, params) {
+              return [200, { msg: 'updated' }];
+            }
+          );
+
+          var item = {};
+          LineItemsService.close(item);
+
+          $httpBackend.flush();
+
+          expect(item.endDate).toBeDefined();
+          expect(item.endDate).toBe(moment().startOf('day').unix());
+        })
+      );
+    });
+
+    describe('LineItemsService', function() {
+      it('should not assign an end date to closed line items if updating failed',
+        inject(function($httpBackend, LineItemsService) {
+          expect(LineItemsService).toBeDefined();
+          expect($httpBackend).toBeDefined();
+
+          $httpBackend.expect('PUT', /\/api\/update\/line-item/).respond(
+            function(method, url, data, headers, params) {
+              return [400, { msg: 'mock error (test)' }];
+            }
+          );
+
+          var item = {};
+          LineItemsService.close(item);
+
+          $httpBackend.flush();
+
+          expect(item.endDate).toBeUndefined();
+        })
+      );
+    });
   });
 })();
