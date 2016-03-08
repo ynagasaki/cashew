210c3c0e6658fe4c891fcdf3d8f05a19baec09cb
Yoshiyuki Nagasaki <yoshinagasaki@yahoo.com>
2016-03-08 00:04:29 -0800

    added chartist and chart WIP

--- app/views/History/History.js
+++ app/views/History/History.js
@@ -14,11 +14,50 @@
   history.controller('HistoryController', ['$scope', 'PaymentsService', function($scope, PaymentsService) {
     var me = this;
     var now = moment().startOf('day');
+    var getGraphData = function(payments) {
+      var result = { labels: [], series: [] };
+      var last = null;
+      var frame = [];
+      payments.forEach(function(payment) {
+        if (last === null || last.year !== payment.year || last.month !== payment.month) {
+          result.labels.push(payment.month + '/' + payment.year);
+          frame.push(0);
+        }
+        frame[frame.length - 1] += payment.amount;
+        last = payment;
+      });
+      result.series.push(frame);
+      return result;
+    };
 
     me.payments = [];
+    me.graph = null;
 
     me.updatePayments = function() {
       me.payments = PaymentsService.payments;
+      me.initGraph(getGraphData(me.payments));
+    };
+    me.initGraph = function(graphData) {
+      if (!me.graph) {
+        me.graph = new Chartist.Bar('#payment-history', {
+          labels: [],
+          series: []
+        }, {
+          stackBars: true,
+          axisY: {
+            labelInterpolationFnc: function(value) {
+              return value;
+            }
+          }
+        }).on('draw', function(data) {
+          if(data.type === 'bar') {
+            data.element.attr({
+              style: 'stroke-width: 30px'
+            });
+          }
+        });
+      }
+      me.graph.update(graphData);
     };
 
     $scope.$on('payments.refreshed', me.updatePayments);
